7a05d44b67d977ff063ac720bb1b3d0c8bd8b519
kidoc <kidoc@DESKTOP-6T3Q9GP>
2022-04-11 23:03:29 -0300

    Add project files.

---
+++ beecrowd media 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+ class progam
+{
+    static void Main(string[] args)
+    {
+        
+        double A , B , media;
+
+         A = double.Parse(Console.ReadLine());
+         B = double.Parse(Console.ReadLine());
+
+         media = ((A * 3.5) + (B * 7.5)) / 11.0;
+
+
+
+
+        Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+        Console.ReadLine();
+
+    }
+
+
+}
